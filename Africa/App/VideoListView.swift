92851326cc380a3a5d552dd1a0b4ee5fcdc6be50
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-29 16:33:02 -0400

    feat: add tabview

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Sean Murphy on 6/29/22.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
